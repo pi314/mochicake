a95e8dd34a005eef326768986e94ecee25ed1a2c
User Pi314 <pi314@quebec.jrc.nctu.edu.tw>
2013-03-04 19:46:54 +0800

    用物件重寫中

---
+++ test.py
@@ -0,0 +1,3 @@
+with open('fishes/P_pi314', 'r') as f:
+    l = ' '.join([i.strip() for i in f]).split()
+    print l
---
+++ tools.py
@@ -0,0 +1,9 @@
+def flatList (inputList, width):
+    maxLength = max([len(i) for i in inputList])
+    ret = []
+    for i in range(0, len(inputList), width):
+        tempList = [j for j in inputList[i:width+i]]
+        tempStr = ' '.join(['{0:{width}}'.format(j, width=maxLength) for j in tempList])
+        ret.append(tempStr)
+    return ret
+
